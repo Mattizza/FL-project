98309cfb4616cf5b232759a6a94c273ea4a3d3bc
JacopoLungo <82226736+JacopoLungo@users.noreply.github.com>
2023-04-22 19:55:15 +0200

    added unNormalize() and showImgAndLable() in idda.py

--- datasets/idda.py
+++ datasets/idda.py
@@ -5,6 +5,7 @@ from PIL import Image
 from torch import from_numpy
 from torchvision.datasets import VisionDataset
 import datasets.ss_transforms as tr
+import matplotlib.pyplot as plt
 
 class_eval = [255, 2, 4, 255, 11, 5, 0, 0, 1, 8, 13, 3, 7, 6, 255, 255, 15, 14, 12, 9, 10]
 
@@ -20,6 +21,7 @@ class IDDADataset(VisionDataset):
         self.list_samples = list_samples
         self.client_name = client_name
         self.target_transform = self.get_mapping()
+        self.root = root
 
     @staticmethod
     def get_mapping():
@@ -31,7 +33,39 @@ class IDDADataset(VisionDataset):
 
     def __getitem__(self, index: int) -> Any:
         # TODO: missing code here!
-        raise NotImplementedError
+        imagePath = os.path.join(self.root, 'images' , self.list_samples[index]+'.jpg')
+        labelPath = os.path.join(self.root, 'labels' ,self.list_samples[index]+'.png')
+
+        image = Image.open(imagePath)
+        label = Image.open(labelPath)
+
+        # ! image e label devono subire la stessa trasformazione di random crop
+        # ! per essere confrontabili nell'evaluation.
+        # ! Entrambi devono essere trasformati in tensore
+        if self.transform is not None:
+            image, label = self.transform(image, label)
+
+        return image, label
 
     def __len__(self) -> int:
         return len(self.list_samples)
+        
+    def unNormalize(self, tensorImage, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+        for t, m, s in zip(tensorImage, mean, std):
+            t.mul_(s).add_(m)
+        return tensorImage
+
+    def showImgAndLable(self, index):
+        image, label = self.__getitem__(index)
+        #unnormalize to show
+        fig, axs = plt.subplots(2)
+        #fig.set_figheight(8.275)
+        #fig.set_figwidth(15)
+        fig.suptitle('Transformed and Segmentated Image')
+        #fig.tight_layout()
+        for i in range(2):
+            axs[i].set_xticks([])
+            axs[i].set_yticks([])
+
+        axs[0].imshow(self.unNormalize(image).permute(1, 2, 0))
+        axs[1].imshow(label.numpy())
