f308cde74e4302786a08245c94c92a829afec9bd
JacopoLungo <jacopo.lungo@hotmail.it>
2023-07-21 15:19:31 +0200

    tidy up files

--- configs/runSingola.py
+++
@@ -1,7 +0,0 @@
-singolaRun = {
-    'optimizer':{'name': 'Adam',
-                 'settings': {'lr':0.005,
-                              'momentum': 0.5}},
-    'scheduler':{'name': 'ExponentialLR',
-                 'settings': {'gamma':0.005}}
-                 }
