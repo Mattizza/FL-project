f89a3041acb2e5458f4722a100a8bea6771a78ba
JacopoLungo <jacopo.lungo@hotmail.it>
2023-08-03 16:42:40 +0200

    improved denormalize and added comments

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
+import numpy as np
+from torchvision.transforms.functional import normalize
 
 class HardNegativeMining(nn.Module):
 
@@ -22,14 +24,26 @@ class MeanReduction:
         x = x[target != 255]
         return x.mean()
 
-def unNormalize(tensorImage, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
-        for t, m, s in zip(tensorImage, mean, std):
+#TODO: eliminare se il programma funziona senza
+#unNormalize non modifica il tensore originale, restituisce una copia unNormalized
+"""def unNormalize(tensorImage, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+        copytensorImage = copy.deepcopy(tensorImage)
+        for t, m, s in zip(copytensorImage, mean, std):
             t.mul_(s).add_(m)
-        return tensorImage
+        return copytensorImage"""
+
+#Non modifica il tensore originale
+def denormalize(tensorImage, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    _mean = -np.array(mean) / std
+    _std = 1 / np.array(std)
+    return normalize(tensorImage, _mean, _std)
+
+
 
 class SelfTrainingLoss(nn.Module):
     requires_reduction = False
-
+    # Più la fraction è bassa più sicura è l'assegnazione della classe.
+    # Con fraction 0.66 avrai sempre almeno il 66% di pixel assegnati a una classe (non 255)
     def __init__(self, conf_th=0.9, fraction=0.66, ignore_index=255, lambda_selftrain=1, **kwargs):
         super().__init__()
         self.conf_th = conf_th
@@ -47,7 +61,7 @@ class SelfTrainingLoss(nn.Module):
         mask_prob = max_prob > self.conf_th if 0. < self.conf_th < 1. else torch.zeros(max_prob.size(),
                                                                                        dtype=torch.bool).to(
             max_prob.device)
-        #crea una mask con solo zero, della stessa dimensione della mask_prob
+        #crea una mask con solo False, della stessa dimensione della mask_prob
         mask_topk = torch.zeros(max_prob.size(), dtype=torch.bool).to(max_prob.device)
 
         if 0. < self.fraction < 1.:
@@ -55,16 +69,20 @@ class SelfTrainingLoss(nn.Module):
                 mask_c = pseudo_lab == c #mask con True dove la label è c
                 max_prob_c = max_prob.clone()
                 max_prob_c[~mask_c] = 0 #crea una mask di probabilità che vale 0 dove non c'è la label c
-                _, idx_c = torch.topk(max_prob_c.flatten(), k=int(mask_c.sum() * self.fraction)) 
+                #Nella riga sotto prende gli indici degli index con prob maggiore nella classe
+                _, idx_c = torch.topk(max_prob_c.flatten(), k=int(mask_c.sum() * self.fraction))
                 mask_topk_c = torch.zeros_like(max_prob_c.flatten(), dtype=torch.bool)
-                mask_topk_c[idx_c] = 1
+                mask_topk_c[idx_c] = 1 #mette true gli indici con prob maggiore
+                #mask_c passa da avere tutti true a true solamente nei pixel con prob maggiore, scelti prima
                 mask_c &= mask_topk_c.unflatten(dim=0, sizes=max_prob_c.size())
-                mask_topk |= mask_c
-        return mask_prob | mask_topk
+                mask_topk |= mask_c #Fa un Or con assegnazione su una mask che all'inizio e solo false,
+                                    #e quindi man mano aggiunge true dei pixel corretti
+        return mask_prob | mask_topk #La barra verticale è un OR. Affinchè un pixel venga selezionato deve essere stato
+                #scelto o dalla mask_prob o dalla mask_top_k
 
     def get_batch_mask(self, pred, pseudo_lab):
         b, _, _, _ = pred.size() #b è la batch size
-        #softmax normalizza le logit. Qundi vengono passate le probabilità e le pseudolabes
+        #softmax normalizza le logit. Qundi vengono passate le probabilità e le pseudolabels
         mask = torch.stack([self.get_image_mask(pb, pl) for pb, pl in zip(F.softmax(pred, dim=1), pseudo_lab)], dim=0)
         return mask
 
@@ -80,7 +98,7 @@ class SelfTrainingLoss(nn.Module):
         else:
             pseudo_lab = pred.detach().max(1)[1]
         mask = self.get_batch_mask(pred, pseudo_lab)
-        pseudo_lab[~mask] = self.ignore_index #dove nella mask c'è il valore 0 mette l'indice da ignorare
+        pseudo_lab[~mask] = self.ignore_index #dove nella mask c'è il valore False mette l'indice da ignorare
         if return_mask_fract:
             return pseudo_lab, F.softmax(pred, dim=1), mask.sum() / mask.numel()
         return pseudo_lab
